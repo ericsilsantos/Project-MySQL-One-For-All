c7e59bfdfa104f749eb5d06aec0907f29ec0720a
Eric Santos <eric.silvasantos44@gmail.com>
2022-06-10 12:05:03 -0300

    fix: requisito 3 e 4

--- desafio4.sql
+++ desafio4.sql
@@ -1,6 +1,6 @@
-SELECT u.usuario AS "usuario",
+SELECT u.name AS "usuario",
 IF (MAX(YEAR(h.data_reproducao)) >= 2021, "Usuário ativo", "Usuário inativo") AS "condicao_usuario"
 FROM SpotifyClone.usuario AS u
 INNER JOIN SpotifyClone.historico_de_reproducoes AS h
-ON u.usuario_id = h.usuario_id
-GROUP BY u.usuario
+ON u.id = h.id
+GROUP BY u.name
