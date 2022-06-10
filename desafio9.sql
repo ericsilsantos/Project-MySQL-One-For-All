2eb79cb5956830a5c23527c1848e8d99e81a1cd9
Eric Santos <eric.silvasantos44@gmail.com>
2022-06-10 12:25:46 -0300

    Adicionado o nono requisito

---
+++ desafio9.sql
@@ -0,0 +1,5 @@
+SELECT COUNT(h.id_cancao) AS "quantidade_musicas_no_historico"
+FROM SpotifyClone.historico_de_reproducoes AS h
+INNER JOIN SpotifyClone.usuario AS u
+ON h.id_usuario = u.id
+WHERE u.name = "Bill";
